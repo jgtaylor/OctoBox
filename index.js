1d68496621fe6ebaf69454daaedb46556a1fa17e
jgtaylor <sonicbphuct@gmail.com>
2017-03-02 20:25:34 +0100

    keys update

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+/* eslint no-unused-vars: "off", no-console: "off" */
+const Later = require("later"),
+	Moment = require("moment"),
+	EventEmitter = require("events"),
+	Http = require("http"),
+	OS = require("os"),
+	Devices = require("./lib/devices");
+var devices = Devices();
+console.log(devices.list());
+devices.save();
