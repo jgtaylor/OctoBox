41acafd15298d492cb6bb96a1be85150551ad099
jgtaylor <sonicbphuct@gmail.com>
2017-11-04 18:09:29 +0100

    remove extra lines

--- index.js
+++ index.js
@@ -3,4 +3,3 @@
 const WS = require("ws"), LM = require("./lib/locationManager.js");
 var devLink = require("./lib/protoParser.js"), lm = LM(), wss = new WS.Server({ port: 2800 });
 lm.addAdapter(wss, devLink);
-
