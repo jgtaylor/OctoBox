27bdb5dadeb3858dde63042534dc31e0fc9bd2d7
jgtaylor <sonicbphuct@gmail.com>
2017-11-06 02:20:27 +0100

    formatting

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 /* eslint no-unused-vars: "off", no-console: "off" */
-const WS = require("ws"), LM = require("./lib/locationManager.js");
-var devLink = require("./lib/protoParser.js"), lm = LM(), wss = new WS.Server({ port: 2800 });
+const WS = require("ws"),
+	LM = require("./lib/locationManager.js");
+var devLink = require("./lib/protoParser.js"),
+	lm = LM(),
+	wss = new WS.Server({
+		port: 2800
+	});
 lm.addAdapter(wss, devLink);
