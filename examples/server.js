7fc5a92e3c60447813260d9387dd566ec2ea7874
jgtaylor <sonicbphuct@gmail.com>
2017-11-07 16:12:46 +0100

    comments

--- examples/server.js
+++ examples/server.js
@@ -25,6 +25,16 @@ lm.addAdapter( wss, devLink ); // associate the WebSocket w the LocationManager
 	include a client here via:
 	var ws = new WS("ws://localhost:2800/some/path",
 		ws.on("open", ws.send(cfg)) );
+
+	Of course, cfg would need to be defined (see the client.js file).
+
+	Ideally, the schedules will be defined/associated via a browser interface,
+	as would the PID controllers. Server starts, devices register via the
+	"config" packet, then the user knows what they've got. They define a zone,
+	or not, assign the registered devices to the zone they belong to, define
+	some schedules for the devices (e.g. light on at 18:00, read temp every 30
+	seconds or so, etc.), then define some PID controllers and associate them
+	with the devices they'll handle. 
  */
 lm.schedules.associate( mySchedule.id, lm.devices.get( "828fbaa2-4f56-4cc5-99bf-57dcb5bd85f5" ), cmd );
 lm.schedules.start( mySchedule.id );
