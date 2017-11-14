d0f1bb458f9e9974897187b6cdb2bc20b6433fe2
jgtaylor <sonicbphuct@gmail.com>
2017-11-14 07:12:14 +0100

    get rid of serialize()

--- index.js
+++ index.js
@@ -6,6 +6,56 @@ var devLink = require( "./lib/protoParser.js" ),
 	lm = LM(),
 	wss = new WS.Server( {
 		port: 2800
+	} ),
+	wssAdmin = new WS.Server( {
+		port: 2801
+	}, () => {
+		// TODO: move this part to a client front end handler stuffy thingy.
+
+		wssAdmin.on( "connection", ( ws, req ) => {
+			console.log( "Connection from: " + req.connection.remoteAddress );
+			this.octoClientIpAddress = req.connection.remoteAddress;
+			ws.on( "message", ( msg ) => {
+				msg = JSON.parse( msg );
+				console.log( "message recieved.", msg );
+
+				switch ( msg[ 0 ] ) {
+				case "client":
+				{
+					switch ( msg[ 1 ].cmd ) {
+					case "register":
+					{
+						console.log( "Registring client..." );
+						ws.send( JSON.stringify( {
+							message: "Registered " + this.octoClientIpAddress,
+							models: lm.devices.list()
+						} ) );
+						break;
+					}
+					case "get":
+					{
+						if ( msg[ 1 ].obj ) {
+							let toSend = lm.devices.get( msg[ 1 ].obj );
+							toSend ? ws.send( toSend.toJSON() ) :
+								ws.send( JSON.stringify( {
+									message: "Error: no device"
+								} ) );
+						}
+						break;
+					}
+					default:
+						console.log( msg[ 1 ].cmd );
+						break;
+					}
+					break;
+				}
+				default:
+					console.log( msg[ 0 ] );
+					break;
+				}
+			}, this );
+		}, this );
+
 	} );
 
 
