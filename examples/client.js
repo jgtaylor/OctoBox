d7acbf4ce1fd7a2601b58de04eea9868987a1cc6
jgtaylor <sonicbphuct@gmail.com>
2017-11-14 07:11:54 +0100

    use 2 clients

--- examples/client.js
+++ examples/client.js
@@ -1,17 +1,16 @@
 "use strict";
 /* eslint no-unused-vars: "off", no-console: "off" */
 /*
-this file is a fake "device" that will connect to a websocket,
-send its configuration over, then send a "reading".
+this file is 2 fake "devices" that will connect to a websocket,
+send its configuration over.
 
-On the server side, you should see 3 devices added, with GUIDs,
-and then you should get a reading object.
+On the server side, you should see 11 devices added, with GUIDs
 */
-const WS = require("ws");
-var cfg = ["config", [{
+const WS = require( "ws" );
+var cfg1 = [ "config", [ {
 	"device": "dccbaa81-b2e4-46e4-a2f4-84d398dd86e3",
 	"type": "virtual",
-	"validCmds": ["read", "readCont"],
+	"validCmds": [ "read", "readCont" ],
 	"meta": {
 		"name": "light",
 		"metric": "light",
@@ -20,16 +19,16 @@ var cfg = ["config", [{
 }, {
 	"device": "828fbaa2-4f56-4cc5-99bf-57dcb5bd85f5",
 	"type": "button",
-	"validCmds": ["on", "off", "getState"],
+	"validCmds": [ "on", "off", "getState" ],
 	"meta": {
 		"usage": "Mains Relay"
 	}
 }, {
 	"device": "c6d2a817-0c3a-4b6f-8478-cd81628a63f5",
 	"type": "virtual",
-	"validCmds": ["read"],
+	"validCmds": [ "read" ],
 	"meta": {
-		"keys": [{
+		"keys": [ {
 			"name": "rh",
 			"metric": "humidity",
 			"unit": "%"
@@ -39,20 +38,87 @@ var cfg = ["config", [{
 			"unit": "C",
 			"validMax": 85,
 			"validMin": -20
-		}],
+		} ],
 		"deviceName": "DHT22"
 	}
-}]];
-var ws = new WS("ws://localhost:2800/devices/josh");
-ws.on("open", () => {
-	ws.send(JSON.stringify(cfg));
-});
-var reading = ["reading", {
-	"device": "c6d2a817-0c3a-4b6f-8478-cd81628a63f5",
-	"value": {
-		"raw": "010000001001101011000000001011110100101010",
-		"rh": 61.9,
-		"temp": 18.9
+} ] ];
+
+var cfg2 = [ "config", [ {
+	"device": "orange-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Orange"
 	}
-}];
-ws.send(JSON.stringify(reading));
+}, {
+	"device": "blue-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Blue"
+	}
+}, {
+	"device": "green-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Green"
+	}
+}, {
+	"device": "red-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Red"
+	}
+}, {
+	"device": "yellow-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Yellow"
+	}
+}, {
+	"device": "purple-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Purple"
+	}
+}, {
+	"device": "white-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "White"
+	}
+}, {
+	"device": "black-84d398dd86e3",
+	"type": "button",
+	"validCmds": [ "on", "off", "getState" ],
+	"meta": {
+		"usage": "Mains Relay",
+		"color": "Black"
+	}
+} ] ];
+var ws1 = new WS( "ws://localhost:2800/devices/josh" );
+ws1.on( "open", () => {
+	ws1.send( JSON.stringify( cfg1 ) );
+	ws1.on( "message", ( msg ) => {
+		console.log( msg );
+	} );
+} );
+var ws2 = new WS( "ws://localhost:2800/devices/josh" );
+ws2.on( "open", () => {
+	ws2.send( JSON.stringify( cfg2 ) );
+	ws2.on( "message", ( msg ) => {
+		console.log( msg );
+	} );
+} );
